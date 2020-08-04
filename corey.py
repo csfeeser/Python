b89863f51124b568be8e534afcc8eeee7d36c22d
Chad Feeser <31425191+csfeeser@users.noreply.github.com>
2020-08-04 18:50:52 -0400

    Create corey.py

---
+++ corey.py
@@ -0,0 +1,56 @@
+{
+	"a1": {
+		"ZONENAME": "At the Jukebox",
+		"DESCRIPTION": "Your at the classic jukebox filled with some of the most classic hits",
+		"EXAMINATION": "You see a coin slot. it reads \"10 cents per song\" you see a forward and back button to select song",
+		"SOLVED": "False",
+		"UP": ["up", "north"],
+		"DOWN": "b1",
+		"LEFT": ["left", "west"],
+		"RIGHT": "a2"
+	},
+	"a2": {
+		"ZONENAME": "At the Booth",
+		"DESCRIPTION": "description",
+		"EXAMINATION": "examine",
+		"SOLVED": "False",
+		"UP": ["up", "north"],
+		"DOWN": ["down", "south"],
+		"LEFT": ["left", "west"],
+		"RIGHT": ["right", "east"]
+	},
+	"b1": {
+		"ZONENAME ": "Inside the Diner ",
+		"DESCRIPTION ": "This is the Jukebox diner",
+		"EXAMINATION ": "examine ",
+		"SOLVED": "False ",
+		"UP ": [
+			"up ",
+			"north "
+		],
+		"DOWN ": [
+			"down ",
+			"south "
+		],
+		"LEFT ": [
+			"left ",
+			"west "
+		],
+		"RIGHT ": [
+			"right ",
+			"east "
+		]
+	},
+	"b2 ": {
+		"ZONENAME ": "Outside the Jukebox Diner ",
+		"DESCRIPTION ": "description ",
+		"EXAMINATION ": "examine ",
+		"SOLVED ": "False",
+		"UP ": "a2 ",
+		"LEFT ": "b1 ",
+		"RIGHT ": [
+			"right ",
+			"east "
+		]
+	}
+}
