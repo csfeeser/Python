784e5cafb45f0c1ba32189dfd16258842da71071
Chase Tucker <tuck8106@gmail.com>
2020-08-10 16:57:30 -0400

    inputs and lists labs complete

---
+++ lists/mixlists.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+# create a list containing three items
+my_list = [ "192.168.0.5", 5060, "UP" ]
+
+# display the first item in the list
+print("The first item in the list (IP): " + my_list[0] )
+
+# display the second item in the list
+print("The second item in the list (port): " + str(my_list[1]) )
+
+# display the third item in the list
+print("The last item in the list (state): " + my_list[2] )
+
+new_list = [ 5060, "80", 55, "10.0.0.1", "10.20.30.1", "ssh" ]
+
+print(f"When I ssh into IP address {new_list[3]} or {new_list[4]} I am unable to ping ports {new_list[0]}, {new_list[1]}, or {new_list[2]}.")
---
+++ scratch/early_lunch.py
@@ -0,0 +1,4 @@
+#!/usr/bin/env python3
+name_input = input("What is your name? ")
+print("Hello {}".format(str(name_input)))
+print("Let me get lunch early!")
