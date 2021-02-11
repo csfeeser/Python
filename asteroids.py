735fda6fe65238d61f7bb36a8be6b430eff0a72a
Chad Feeser <31425191+csfeeser@users.noreply.github.com>
2021-02-11 15:10:44 -0500

    Update asteroids.py

--- asteroids.py
+++ asteroids.py
@@ -1,29 +1,36 @@
-#!/usr/bin/python3
-import requests
+#!/usr/bin/env python3
+  
+import requests ## 3rd party URL lookup
 
-## Define NEOW URL 
-NEOURL = "https://api.nasa.gov/neo/rest/v1/feed?"
-
+## define the main function
 def main():
-    with open("/home/student/nasa.creds", "r") as mycreds:
-        nasacreds = mycreds.read()
-    nasacreds = "api_key=" + nasacreds.strip("\n")
-    startdate = "start_date=2019-11-11"
-    neowrequest = requests.get(NEOURL + startdate + "&" + nasacreds)
-    neodata = neowrequest.json()
+    neourl = 'https://api.nasa.gov/neo/rest/v1/feed?' # API URL
 
-    asteroid_sizes= {}
+    startdate= "start_date=" + input("Choose a start date (YYYY-MM-DD) ")
+    enddate= "&end_date=" + input("Choose an end date (YYYY-MM-DD) ")
+    mykey = '&api_key=g2dpPgvaIy7V4gdXO8cTKmMMJP2EEwR3RNYb6aQ0' ## replace this with our API key
+
+    neourl = neourl + startdate + enddate + mykey
+    print(neourl)
+
+    neodata = (requests.get(neourl)).json()
+
+    sizes= []
+    asteroid_distance= []
     asteroid_names= []
     danger_count= 0
+
     print(f"Total Asteroids in Range: {neodata['element_count']}")
     for date in neodata["near_earth_objects"].keys():
         for aster in neodata["near_earth_objects"][date]:
-            asteroid_sizes[aster["name"]]= aster["estimated_diameter"]["meters"]["estimated_diameter_max"]
+            sizes.append(aster["estimated_diameter"]["kilometers"]["estimated_diameter_max"])
             asteroid_names.append(aster["name"])
+            asteroid_distance.append(aster["close_approach_data"][0]["miss_distance"]["lunar"])
             if aster["is_potentially_hazardous_asteroid"]:
                 danger_count += 1
 
-    print(f"Largest Asteroid (meters): {max(asteroid_sizes)}")
+    print(f"Largest Asteroid (kilometers): {max(sizes)}")
+    print(f"Closest Asteroid (lunar): {max(asteroid_distance)}")
     print(f"Number of potentially hazardous asteroids: {danger_count}")
     input("Press ENTER to see a list of all asteroids.")
     print(asteroid_names)
