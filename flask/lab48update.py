f547a80e26d9458fd0db082e9412e26f471cddca
Chad Feeser <31425191+csfeeser@users.noreply.github.com>
2021-02-25 17:15:06 -0500

    Update lab48update.py

--- flask/lab48update.py
+++ flask/lab48update.py
@@ -89,6 +89,6 @@ if __name__ == '__main__':
         conn.execute('CREATE TABLE IF NOT EXISTS students (name TEXT, addr TEXT, city TEXT, pin TEXT)')
         print("Table created successfully")
         conn.close()
-        app.run(debug=True)
+        app.run(host="0.0.0.0", port=2224, debug = True)
     except Exception:
         print("App failed on boot")
