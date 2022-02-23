9e3da4793abaf80e747257ece8f2a18eba2819c1
Chad Feeser <31425191+csfeeser@users.noreply.github.com>
2022-02-23 10:45:00 -0500

    Create simpsonslice.py

---
+++ downloads/simpsonslice.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+"""Morning Slicing Challenge!"""
+
+challenge= ["science", "turbo", ["goggles", "eyes"], "nothing"]
+
+trial= ["science", "turbo", {"eyes": "goggles", "goggles": "eyes"}, "nothing"]
+
+nightmare= [{"slappy": "a", "text": "b", "kumquat": "goggles", "user":{"awesome": "c", "name": {"first": "eyes", "last": "toes"}},"banana": 15, "d": "nothing"}]
+
+def main():
+    """write your code in this function to solve the challenge"""
+
+    # write one print function for each list above
+    # get the strings "eyes," "goggles," and "nothing from each
+    # final output for each print should look like this:
+    """My eyes! The goggles do nothing!"""
+
+    # print()
+    # print()
+    # print()
+
+if __name__ == "__main__": # <-- what is this??? we will review later :)
+    main()
