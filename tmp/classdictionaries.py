64f296e36eda286e79a8d779af9c453ef591aa0b
Chad Feeser <31425191+csfeeser@users.noreply.github.com>
2023-04-26 11:13:57 -0400

    Create classdictionaries.py

---
+++ tmp/classdictionaries.py
@@ -0,0 +1,118 @@
+lucas_mccain = {
+    'Occupations': ['homesteader', 'rancher', 'sodbuster',
+                    'fighting bad guys'],
+    'weapons': 'modified Winchester Model 1892 rifle',
+    'children': 'Mark McCain',
+    'Aliases': ['McCain', 'sodbuster', 'Luke', 'Lucas-boy'],
+    }
+
+ryan_stock = {'tv shows': [
+    'Guinea Pig',
+    'Jim Rose Twisted Tour',
+    'Jim Rose Circus',
+    'Tonight Show',
+    'Guiness World Records Gone Wild',
+    "America's Got Talent",
+    'Lo Show dei Record',
+    ],
+        'world records': ['Heaviest vehicle pulled using a hook through the nasal cavity and out of the mouth'
+                          ,
+                          'Most drink can tops torn off with the teeth'
+                          ,
+                          'Most blowtorches extinguished on the tongue in one minute'
+                          ,
+                          'The heaviest weight lifted by hooks in the forearms'
+                          ],
+        'newest achievement': {'feat': '56 cm in 1/12'}}
+
+luke_skywalker = {
+    'Title': ['Jedi Knight', 'Jedi Master', 'Red Five', 'Rogue Leader'
+              ],
+    'Occupation': ['Apprentice', 'Moisture Farmer', 'Pilot', 'Jedi'],
+    'Affiliation': [
+        'Skywalker family',
+        'Rebel Alliance',
+        'Rogue Squadron',
+        'Jedi',
+        'New Republic',
+        'New Jedi Order',
+        'Resistance',
+        'Galactic Alliance',
+        'Jedi Council',
+        ],
+    'Family': ['Padm\xc3\xa9 Amidala (mother)',
+               'Anakin Skywalker (father)', 'Leia Organa (twin sister)'
+               , 'Owen Lars (paternal step-uncle)',
+               'Beru Lars (paternal step-aunt)'],
+    }
+
+theGambler = {
+    'name': 'Mat Cauthon',
+    'nationality': 'Andoran',
+    'birth year': 978,
+    'status': 'alive',
+    'abilities': ['Extreme Luck', 'Fighting Skills',
+                  'Military Expertise'],
+    }
+
+elvis_presley = {
+    'birthday': 'january-8-1935',
+    'place_of_birth': 'tupelo',
+    'death': 'august-16-1977',
+    'AKA': 'The King',
+    }
+
+patrick_star = {
+    'species': 'Starfish',
+    'occupation': 'Unemployed',
+    'residence': 'Bikini Bottom',
+    'best_friend': 'SpongeBob SquarePants',
+    'hobbies': ['sleeping', 'eating', 'rock collecting'],
+    }
+
+kingJames = {
+    'Alter Ego': 'LeBron James',
+    'Origin': 'Akron, Ohio',
+    'Powers': ['Longevity', 'Basketball IQ', 'Handles', 'Shot selection'
+               ],
+    'Ranking': ['Leading Scorer-Regular Season',
+                'Leading Scorer-Post Season'],
+    }
+
+tiger_woods = {
+    'name': 'Tiger Woods',
+    'age': 46,
+    'profession': 'Professional Golfer',
+    'achievements': ['15 major championships', '82 PGA Tour wins',
+                     '11-time PGA Tour Player of the Year',
+                     '2-time FedEx Cup Champion'],
+    }
+
+pianist = {
+    'full_name': 'Sergei Rachmaninoff',
+    'DOB': '1 April 1873',
+    'DOD': '28 March 1943',
+    'profession': ['Composer', 'Pianist'],
+    'famous_works': ['Prelude in C#minor', 'Prelude in Gminor',
+                     'Piano Concerto No. 2 in C minor'],
+    }
+
+warbird = {
+    'Alter Ego': 'Carol Danvers',
+    'Origin': 'Boston, Massachusetts',
+    'Powers': ['Super Human Strength', 'Super Human Durability',
+               'Flight', 'Energy Blast'],
+    'Relationships': {'ally': 'Capt America', 'Enemy': 'Thanos'},
+    }
+
+sam_winchester = {
+    'Nickname': ['Sam', 'Sammy', 'B---h', 'Moose'],
+    'Species': ['Human', 'Ghost', 'Rabid'],
+    'Gender': 'Male',
+    'Occupation': ['Hunter', 'Witch-in-training',
+                   ['Former Undergraduate',
+                   'Former law school applicant']],
+    'Family Members': {'Parents': {'Mother': 'Mary Winchester',
+                       'Father': 'John Winchester'},
+                       'Siblings': {'Younger Brother': 'Adam Milligan'}},
+    }
