7eba10fe42a92cdb2ebb6fe6cb8365f0a0cc87eb
Chad Feeser <31425191+csfeeser@users.noreply.github.com>
2023-05-01 15:50:04 -0400

    Update pokedex_csv.py

--- data sets/demo code/pokedex_csv.py
+++ data sets/demo code/pokedex_csv.py
@@ -1,5 +1,5 @@
 import csv
   
-with open("pokedex.txt") as pokedata:
+with open("pokedex_gen1.txt") as pokedata:
     for x in csv.DictReader(pokedata):
         print(f'{x["Name"]} has an attack score of {x["Attack"]}.')
